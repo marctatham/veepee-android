37d182f18617fc90cc4af7c57ad88d3063ea2329
Marc Tatham <marctatham@gmail.com>
2023-08-20 13:27:43 +0200

    added Repository & entity definition for Favorites

---
+++ data/favorites/src/main/java/com/vp/data/favorites/FavoriteRepository.kt
@@ -0,0 +1,14 @@
+package com.vp.data.favorites
+
+import androidx.lifecycle.LiveData
+
+interface FavoriteRepository {
+
+    fun observeAll(): LiveData<List<Favorite>>
+
+    fun observeById(id: String): LiveData<Favorite>
+
+    suspend fun addFavorite(favorite: Favorite)
+
+    suspend fun delete(id: Favorite): Int
+}
