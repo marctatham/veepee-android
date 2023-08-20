8285bb8cfed9d779ce9c2a41df3ece7f8fd69c1d
Marc Tatham <marctatham@gmail.com>
2023-08-20 13:59:05 +0200

    added mapper dependency
    *should have been included in previous commit

--- data/favorites/src/main/java/com/vp/data/favorites/local/mapper/FavoriteDataModelMapper.kt
+++ data/favorites/src/main/java/com/vp/data/favorites/local/mapper/FavoriteDataModelMapper.kt
@@ -2,13 +2,19 @@ package com.vp.data.favorites.local.mapper
 
 import com.vp.data.favorites.Favorite
 import com.vp.data.favorites.local.model.FavoriteDataModel
+import javax.inject.Inject
 
-class FavoriteDataModelMapper {
+class FavoriteDataModelMapper @Inject constructor() {
 
-    fun map(dataModel: FavoriteDataModel): Favorite = Favorite(
+    fun mapTo(dataModel: FavoriteDataModel): Favorite = Favorite(
         dataModel.id,
         dataModel.title,
         dataModel.posterUrl
     )
 
+    fun mapFrom(favorite: Favorite): FavoriteDataModel = FavoriteDataModel(
+        favorite.id,
+        favorite.title,
+        favorite.posterUrl
+    )
 }
