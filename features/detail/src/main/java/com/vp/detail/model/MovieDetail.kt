0830d16a00ce952dd962bcdbcbad6a7db08d0858
Marc Tatham <marctatham@gmail.com>
2023-08-20 14:48:04 +0200

    introduced new "id" field

--- features/detail/src/main/java/com/vp/detail/model/MovieDetail.kt
+++ features/detail/src/main/java/com/vp/detail/model/MovieDetail.kt
@@ -3,6 +3,7 @@ package com.vp.detail.model
 import com.google.gson.annotations.SerializedName
 
 data class MovieDetail(
+    @SerializedName("imdbID") val id: String,
     @SerializedName("Title") val title: String,
     @SerializedName("Year") val year: String,
     @SerializedName("Runtime") val runtime: String,
